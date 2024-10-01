9f3bfaa8ca66bd151dab77818197fa4e41ca09d9
Kleinbard <kkleinbard@avaya.com>
2024-09-30 20:38:14 -0400

    new numbers counters

--- orchestration/initializeCharts.js
+++ orchestration/initializeCharts.js
@@ -4,15 +4,11 @@ import { typeWriter } from "../charts/animatedText.js";
 import { createAnimatedPieChart } from "../charts/pieCircle.js";
 import {createHandleTimeGraph} from "../charts/linegraph_static.js"
 import {createAgentWellnessChart} from "../charts/multiArea.js"
-// import { orchestrateScrolling, stopScrolling } from "./scroller";
-import { configs } from "../configs/configs";
 
 
 export function initializeCharts(data = null) {
-  // currentData = data  // create global context
-  // if (data && isScroll) {
-  //   scrollHelper(data, configs.scrollInterval);
-  // }
+  console.log("i am data", data)
+
   manualUpdate(data?.counters || 1);
   typeWriter(data?.antimatedText);
  
@@ -21,17 +17,30 @@ export function initializeCharts(data = null) {
   const barChart = createBarChart(data?.bar);
   const barPanel = document.querySelector("#bar-chart-panel");
   const stackAreaPanel = document.querySelector("#multi-area-panel");
+  const emergencyFlow = document.querySelector("#emergency_workflow2")
+  const normalFlow = document.querySelector("#emergency_workflow_non")
+
   createAgentWellnessChart(stackAreaPanel, data?.multiArea || 1)
 
   barPanel.innerHTML = "";
   barPanel.appendChild(barChart);
 
   const lineGraphStaticPanel = document.querySelector("#line-graph-static-panel");  
-  const lineGraphStatic = createHandleTimeGraph(lineGraphStaticPanel, data?.lineGraphStatic || 1)
+  const __ = createHandleTimeGraph(lineGraphStaticPanel, data?.lineGraphStatic || 1)
 
   const pieChartContReal = document.getElementById("pie-panel");
-  const pieChartCont = createAnimatedPieChart(
+  const _ = createAnimatedPieChart(
     pieChartContReal,
     data?.pieChart || 1
   );
+
+  if(data?.workflow === '1')
+  {
+    emergencyFlow.style.display = 'block';
+    normalFlow.style.display = 'none';
+  }
+  else{
+    emergencyFlow.style.display = 'none';
+    normalFlow.style.display = 'block';
+  }
 }
